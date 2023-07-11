0f490e2fc37c3fb7a5a9d7f7ddac9292ac3b84e8
vinaya-sv <95991141+vinaya-sv@users.noreply.github.com>
2023-07-11 15:16:48 +0530

    App Development

---
+++ Day 2 - SignIn and SignUp Page/sign_in.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import './css/sign_in.css';
+import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { login ,logout} from './features/reducer';
+import Home from './home';
+function SignIn(){
+    const dispatch=useDispatch();
+    const navigate=useNavigate();
+    const [username,setusername]=useState("");
+    const [password,setpassword]=useState("");
+    return(
+        // <div className="sign-in-outer">
+            <div className="sign-in-card">
+                <form>
+                <h1 className="sign-in-label">Sign In</h1>
+                <div className="input-field-area">
+                    <input className="input-field" value={username} onChange={e=>setusername(e.target.value)} type="text" placeholder="Enter your username" required autoComplete='off'></input>
+                    <input className="input-field" value={password} onChange={e=>setpassword(e.target.value)} type="password" placeholder="Enter your password" required autoComplete='off'></input>
+                </div>
+                <button onClick={()=>{dispatch(login({username,password}));navigate("/home")}} className="submit-button" type="submit"><b>Submit</b></button>
+                <Link to="/signup"><p className="text">Don't have an account? Sign up</p></Link>
+                </form>
+            </div>
+        // </div>
+    ); 
+}
+export default SignIn;
